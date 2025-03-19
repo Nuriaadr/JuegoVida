712b7ec82b93f7779b3a4e779c7bdd88ebd3b7bb
ralcara908 <ralcara908@g.educaand.es>
2025-03-19 14:43:42 +0100

    menus correctos

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -13,56 +13,69 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-  Scanner sc = new Scanner(System.in);
-        boolean repetir;
+      Scanner sc = new Scanner(System.in);
+        boolean repetir = true;
         int numero = 0;
         int[][] tablero = new int[numero][numero];
         JuegoVida juego = new JuegoVida(numero);
-        do {
 
-            System.out.println("Ingrese tamaño del tablero (maximo 25x25):");
-            numero = sc.nextInt();
-            sc.nextLine();
-
-            if (numero < 1 || numero > 25) {
-                System.out.println("Introduzca un número válido");
-                repetir = true;
-            } else {
-                JuegoVida juego = new JuegoVida(numero);
-                System.out.println("Ingrese el porcentaje de células vivas:");
-                int porcentaje = sc.nextInt();
-                if (porcentaje < 1 || porcentaje > 100) {
-                    System.out.println("Introduzca un porcentaje válido");
-                    repetir = true;
-                } else {
-                    juego.inicializarAleatoriamente(porcentaje);
-                    System.out.println("Tablero inicializado");
-                    repetir = false;
-                }
-            }
-
-        } while (repetir == true);
+         String menu = """
+            Menu
+            1. Cargar una partida desde un fichero de texto plano
+            2. Iniciar un nuevo juego
+            3. Mostrar siguiente generacion
+            4. Guardar partida
+            5. Salir
+            Elige una opcion: 
+            """;
 
         String opcion;
         do {
-            System.out.println("""
-          Menu
-    1. Mostrar siguiente generacion
-    2. Salir
-    Elige una opcion: 
-                     """);
+            System.out.print(menu);
             opcion = sc.nextLine();
+
             switch (opcion) {
                 case "1":
+                    System.out.println("Cargando partida desde archivo...");
+                   
+                    break;
 
+                case "2":
+                 
+                    do {
+                        System.out.println("Introduce tamaño del tablero (maximo 25x25)");
+                        numero = sc.nextInt();
+                        if (numero < 1 || numero > 25) {
+                            System.out.println("Introduce el numero correctamente");
+                        } else {
+                            System.out.println("Introduce el porcentaje ");
+                            int porcentaje = sc.nextInt();
+                            if (porcentaje < 1 || porcentaje > 100) {
+                                System.out.println("Introduce el porcentaje correctamente");
+                            } else {
+                                juego.inicializarAleatoriamente(porcentaje);
+                                System.out.println("Tablero inicializado");
+                                repetir = false;
+                            }
+                        }
+                    } while (repetir);
+                    break;
+
+                case "3":
+                    System.out.println("Mostrando siguiente generacion...");
                     juego.mostrarTablero();
                     break;
-                case "2":
+
+                case "4":
+                    
+                    System.out.println("Guardando partida...");
+                   
+                    break;
+
+                case "5":
                     System.out.println("Saliendo...");
                     break;
             }
-        } while (!opcion.equals("2"));
+        } while (!opcion.equals("5"));
     }
 }
-
-
